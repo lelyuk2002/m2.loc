c7ba3b4741f683949a0b875e8a6a18bf0ee2d183
sasha <lelyk2002@gmail.com>
2022-06-08 17:31:47 +0300

    plugin which change name

---
+++ app/code/Codilar/Demo/Plugins/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Codilar\Demo\Plugins;
+
+class Product
+{
+
+    public function aftergetName(\Magento\Catalog\Model\Product $product, $name){
+        $price = $product->getData('price');
+        if($price < 10){
+            $name .= ' So cheap';
+        }
+        else {
+            $name .= 'So expensive';
+        }
+
+        return $name;
+    }
+
+}
